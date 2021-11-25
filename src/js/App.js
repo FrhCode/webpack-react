940000c851d04e75e7b66cd6d2bfe8fbc6bc067f
farhan <farhan7534031b@gmail.com>
2021-11-25 15:53:57 +0700

    initial commit

---
+++ src/js/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Welcome from "./component/Welcome/welcome";
+
+export default class App extends React.Component {
+    render() {
+        return <h1>Hello Wolrd</h1>;
+    }
+}
