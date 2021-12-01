cbb8bb4ff33c0b5ab8379beedd67d6c8ad6f667c
Farhan <48715452+MohammadFarhan7534031b@users.noreply.github.com>
2021-12-01 19:15:03 +0700

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const appElement = document.getElementById("app");
+const appElement = document.getElementById("root");
 
 ReactDOM.render(<App />, appElement);
