65bbf2ec1b683ef6b8f223ecd20f2bb78bad2079
Farhan <48715452+MohammadFarhan7534031b@users.noreply.github.com>
2021-12-01 19:16:14 +0700

    Update App.js

--- src/js/App.js
+++ src/js/App.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default class App extends React.Component {
     render() {
-        return <h1>Hello Wolrd</h1>;
+        return (
+            <div className={"App"}>
+                <h1>Hello Wolrd</h1>
+            </div>
+        );
     }
 }
